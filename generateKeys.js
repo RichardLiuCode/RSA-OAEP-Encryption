3f37a772cfbd5c8ca39873f3ee63d5195430754e
Richard Liu <richardadesigner@gmail.com>
2026-02-07 10:42:46 -0800

    update

--- generateKeys.js
+++ generateKeys.js
@@ -13,14 +13,14 @@ document.getElementById("generateKeyBtn").addEventListener("click", function () 
                     const exportedAsString = String.fromCharCode.apply(null, new Uint8Array(exportedPublicKey));
                     const exportedAsBase64 = btoa(exportedAsString);
                     const FullExportedPublicKey = "-----BEGIN PUBLIC KEY-----" + "\n" + exportedAsBase64 + "\n" + "-----END PUBLIC KEY-----";
-                    console.log(FullExportedPublicKey)
+                    document.getElementById("publicKeyDisplayArea").innerText = FullExportedPublicKey;
                 });
             window.crypto.subtle.exportKey("pkcs8", keyPair.privateKey)
                 .then(function (exportedPrivateKey) {
                     const exportedAsString = String.fromCharCode.apply(null, new Uint8Array(exportedPrivateKey));
                     const exportedAsBase64 = btoa(exportedAsString);
                     const FullExportedPrivateKey = "-----BEGIN PRIVATE KEY-----" + "\n" + exportedAsBase64 + "\n" + "-----END PRIVATE KEY-----";
-                    console.log(FullExportedPrivateKey);
+                    document.getElementById("privateKeyDisplayArea").innerText = FullExportedPrivateKey;
                 })
         })
 });
