558fcfea24e87e8da8d529c7088dd417f716c60d
Richard Liu <richardadesigner@gmail.com>
2026-02-17 08:47:09 -0800

    update

--- decrypt.js
+++ decrypt.js
@@ -14,7 +14,7 @@ document.getElementById("privateKey").addEventListener("input", function () {
     localStorageData.inputPrivateKeyForDecrypt = document.getElementById("privateKey").value;
     localStorage.setItem("Project:RSA-OAEP-Encryption", JSON.stringify(localStorageData));
 });
-document.getElementById("encryptBtn").addEventListener("click", function () {
+document.getElementById("decryptBtn").addEventListener("click", function () {
     const encryptedMessage = document.getElementById("input").value;
     const binaryString = atob(encryptedMessage);
     const bytes = new Uint8Array(binaryString.length);
@@ -22,37 +22,61 @@ document.getElementById("encryptBtn").addEventListener("click", function () {
         bytes[i] = binaryString.charCodeAt(i);
     }
     const encryptedBuffer = bytes.buffer;
+
+    /* --------------------------------- */
+    const displayError = function () {
+        document.getElementById("status").innerText = "Error";
+        setTimeout(function () {
+            document.getElementById("status").innerText = "";
+        }, 2000);
+    };
     /* --------------------------------- */
     const privateKeyPem = document.getElementById("privateKey").value;
     const pemHeader = "-----BEGIN PRIVATE KEY-----";
     const pemFooter = "-----END PRIVATE KEY-----";
-    const pemContents = privateKeyPem.substring(pemHeader.length, privateKeyPem.length - pemFooter.length).replace("\n", "").replace("\n", "");
-    const binaryDerString = atob(pemContents);
-    const buf = new ArrayBuffer(binaryDerString.length);
-    const bufView = new Uint8Array(buf);
-    for (let i = 0; i < binaryDerString.length; i++) {
-        bufView[i] = binaryDerString.charCodeAt(i);
+    if (!privateKeyPem) {
+        return;
     }
-    window.crypto.subtle.importKey(
-        "pkcs8",
-        buf,
-        {
-            name: "RSA-OAEP",
-            hash: "SHA-256"
-        },
-        true,
-        ["decrypt"])
-        .then(function (importedPrivateKey) {
-            window.crypto.subtle.decrypt(
-                {
-                    name: "RSA-OAEP"
-                },
-                importedPrivateKey,
-                encryptedBuffer
-            )
-                .then(function (decryptedMessage) {
-                    const decoder = new TextDecoder();
-                    document.getElementById("result").value = decoder.decode(decryptedMessage);
-                });
-        });
+    if (privateKeyPem.substring(0, 27) == pemHeader && privateKeyPem.substring(privateKeyPem.length, privateKeyPem.length - 25) == pemFooter) {
+        const pemContents = privateKeyPem.substring(pemHeader.length, privateKeyPem.length - pemFooter.length).replace("\n", "").replace("\n", "");
+        const binaryDerString = atob(pemContents);
+        const buf = new ArrayBuffer(binaryDerString.length);
+        const bufView = new Uint8Array(buf);
+        for (let i = 0; i < binaryDerString.length; i++) {
+            bufView[i] = binaryDerString.charCodeAt(i);
+        }
+        window.crypto.subtle.importKey(
+            "pkcs8",
+            buf,
+            {
+                name: "RSA-OAEP",
+                hash: "SHA-256"
+            },
+            true,
+            ["decrypt"])
+            .then(function (importedPrivateKey) {
+                window.crypto.subtle.decrypt(
+                    {
+                        name: "RSA-OAEP"
+                    },
+                    importedPrivateKey,
+                    encryptedBuffer
+                )
+                    .then(function (decryptedMessage) {
+                        const decoder = new TextDecoder();
+                        document.getElementById("result").value = decoder.decode(decryptedMessage);
+                    })
+                    .catch(function (error) {
+                        displayError();
+                    });
+            })
+            .catch(function (error) {
+                displayError();
+            });
+    } else {
+        document.getElementById("status").innerText = "Invalid Private Key";
+        setTimeout(function () {
+            document.getElementById("status").innerText = "";
+        }, 2000);
+    }
 });
